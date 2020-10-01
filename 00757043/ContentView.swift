9f729307fa8c48a221e11abc6684f7a8c0fe5489
aaaaaaaaaaa <aaaaa>
2020-10-01 19:15:39 +0800

    line2

--- 00757043/ContentView.swift
+++ 00757043/ContentView.swift
@@ -45,8 +45,40 @@ struct ContentView: View {
             Path{(path) in
                 path.move(to: CGPoint(x:65, y:61))
                 path.addQuadCurve(to: CGPoint(x:43, y:106), control: CGPoint(x:49, y:80))
-            }.stroke(lineWidth: 2)
-        }
+            }.stroke(lineWidth: 2)   //outline
+            Path{(path) in
+                path.move(to: CGPoint(x:34, y:147))
+                path.addQuadCurve(to: CGPoint(x:83, y:227), control: CGPoint(x:39, y:213))
+                path.addCurve(to: CGPoint(x: 50, y: 87), control1: CGPoint(x: 73, y: 194), control2: CGPoint(x: 40, y: 152))
+            }.stroke(lineWidth: 0.5)
+            Path{(path) in
+                path.move(to: CGPoint(x: 60, y: 172))
+                path.addQuadCurve(to: CGPoint(x: 94, y: 55), control: CGPoint(x: 62, y: 83))
+                path.move(to: CGPoint(x: 123, y: 74))
+                path.addQuadCurve(to: CGPoint(x: 151, y: 126), control: CGPoint(x: 124, y: 116))
+                path.move(to: CGPoint(x: 168, y: 61))
+                path.addQuadCurve(to: CGPoint(x: 202, y: 173), control: CGPoint(x: 196, y: 104))
+                path.move(to:CGPoint(x: 210, y: 93))
+                path.addCurve(to: CGPoint(x: 179, y: 230), control1: CGPoint(x: 219, y:188), control2: CGPoint(x:177, y:194))
+            }.stroke(lineWidth: 0.5) //xi
+            Path{(path) in
+                path.move(to: CGPoint(x: 62, y: 139))
+                path.addQuadCurve(to: CGPoint(x: 75, y: 105), control: CGPoint(x: 70, y: 128))
+                path.addQuadCurve(to: CGPoint(x: 90, y: 130), control: CGPoint(x: 74, y: 129)) //75123
+                path.addQuadCurve(to: CGPoint(x: 90, y: 123), control: CGPoint(x: 88, y: 126))
+                path.move(to: CGPoint(x: 87, y: 101))
+                path.addQuadCurve(to: CGPoint(x: 98, y: 133), control: CGPoint(x: 87, y: 126))
+                path.addQuadCurve(to: CGPoint(x: 104, y: 87), control: CGPoint(x: 92, y: 108))
+                path.move(to: CGPoint(x: 114, y: 59))
+                path.addQuadCurve(to: CGPoint(x: 105, y: 103), control: CGPoint(x: 101, y: 81))
+                path.addQuadCurve(to: CGPoint(x: 149, y: 125), control: CGPoint(x: 116, y: 132))
+                path.addQuadCurve(to: CGPoint(x: 145, y: 86), control: CGPoint(x: 141, y: 105))
+                path.addQuadCurve(to: CGPoint(x: 178, y: 124), control: CGPoint(x: 152, y: 127))
+                path.move(to: CGPoint(x: 181, y: 125))
+                path.addQuadCurve(to: CGPoint(x: 166, y: 93), control: CGPoint(x: 169, y: 121))
+                path.addQuadCurve(to: CGPoint(x: 195, y: 127), control: CGPoint(x: 175, y: 119))
+            }.stroke(lineWidth: 1) //kami
+        }//42 210
 
 
         
